e8767114e47f0d281f62e065d8226f39b93b61ac
Sixseven-6677 <maldy7478@gmail.com>
2026-04-19 22:25:16 +0200

    Simplify start.sh

--- start.sh
+++ start.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
-
 cd "$(dirname "$0")"
-echo "Working dir: $(pwd)"
-
-if [ ! -z "$APPSTATE" ]; then
-  echo "$APPSTATE" > appstate.json
-  echo "✓ appstate.json created"
-fi
-
-if [ ! -z "$CONFIG" ]; then
-  echo "$CONFIG" > config.json
-  echo "✓ config.json created"
-fi
-
-echo "Installing root dependencies..."
-npm install --no-package-lock --silent
-
-echo "Installing lib/fca-auto dependencies..."
-cd lib/fca-auto && npm install --no-package-lock --silent && cd ../..
-
+[ ! -z "$APPSTATE" ] && echo "$APPSTATE" > appstate.json && echo "✓ appstate.json created"
+[ ! -z "$CONFIG" ] && echo "$CONFIG" > config.json && echo "✓ config.json created"
 echo "Starting bot..."
 node index.js
