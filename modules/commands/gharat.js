2f54f39de40c0e320dc123e0023d76e547f40fc4
Sixseven-6677 <maldy7478@gmail.com>
2026-05-09 22:49:37 +0200

    fix: gharat.js wrong dataDir path

--- modules/commands/gharat.js
+++ modules/commands/gharat.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const dataDir = path.join(__dirname, "data");
+const dataDir = path.join(process.cwd(), "Horizon_Database");
 const playersPath = path.join(dataDir, "qetal_players.json");
 
 const gharatBattles = global.gharatBattles || (global.gharatBattles = new Map());
