20ce4b3e821d5fa8e73c9251795473d3ec2f95da
✻ Mυິhαׅׄm︩︪ɐิძ 🇰🇷🦢 <maldy7478@gmail.com>
2026-05-16 20:25:08 +0200

    fix(antibd): remove double var nickname declaration

--- modules/events/antibd.js
+++ modules/events/antibd.js
@@ -1,7 +1,7 @@
 module.exports.config = {
   name: "antibd",
   eventType: ["log:user-nickname"],
-  version: "0.0.1",//beta
+  version: "0.0.2",
   credits: "ProCoderCyrus",
   description: "Chống đổi biệt danh của Bot"
 };
@@ -10,11 +10,16 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     var { logMessageData, threadID, author } = event;
     var botID = api.getCurrentUserID();
     var { BOTNAME, ADMINBOT } = global.config;
-    var { nickname } = await Threads.getData(threadID, botID);
-    var nickname = nickname ? nickname : `『 ${global.config.PREFIX} 』 ⪼ ${global.config.BOTNAME}`;
+
+    // BUG FIX: single clean declaration — Threads.getData returns thread data object
+    var threadData = await Threads.getData(threadID);
+    var nickname = (threadData && threadData.nickname)
+      ? threadData.nickname
+      : `『 ${global.config.PREFIX} 』 ⪼ ${global.config.BOTNAME}`;
+
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
-        api.changeNickname(nickname, threadID, botID)
+        api.changeNickname(nickname, threadID, botID);
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - 𝐁𝐚̣𝐧 𝐊𝐡𝐨̂𝐧𝐠 𝐂𝐨́ 𝐐𝐮𝐲𝐞̂̀𝐧!!!`}, threadID);
-    }  
-}
+        return api.sendMessage({ body: `${info.name} - 𝐁𝐚̣𝐧 𝐊𝐡𝐨̂𝐧𝐠 𝐂𝐨́ 𝐐𝐮𝐲𝐞̂̀𝐧!!!` }, threadID);
+    }
+};
