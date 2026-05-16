db1da2d2d13e93156bfa373b8bcc7217c7ac6465
✻ Mυິhαׅׄm︩︪ɐิძ 🇰🇷🦢 <maldy7478@gmail.com>
2026-05-16 20:18:07 +0200

    fix(log): check only current thread log setting, fix timezone

--- modules/events/log.js
+++ modules/events/log.js
@@ -1,7 +1,7 @@
 module.exports.config = {
     name: "log",
     eventType: ["log:unsubscribe", "log:subscribe", "log:thread-name"],
-    version: "1.0.0",
+    version: "1.0.1",
     credits: "Mirai Team",
     description: "Ghi lại thông báo các hoạt đông của bot!",
     envConfig: {
@@ -13,27 +13,26 @@ module.exports.config = {
     const logger = require("../../utils/log");
     if (!global.configModule[this.config.name].enable) return;
     let botID = api.getCurrentUserID();
-    var allThreadID = global.data.allThreadID;
-    for (const singleThread of allThreadID) {
-      const thread = global.data.threadData.get(singleThread) || {};
-      if (typeof thread["log"] != "undefined" && thread["log"] == false) return;
-    }
+
+    // BUG FIX: check only the current thread's log setting, not all threads
+    const thread = global.data.threadData.get(event.threadID) || {};
+    if (typeof thread["log"] !== "undefined" && thread["log"] === false) return;
     
     const moment = require("moment-timezone");
     const time = moment.tz("Asia/Ho_Chi_Minh").format("D/MM/YYYY HH:mm:ss");
-    //let nameThread = (await Threads.getData(event.threadID)).threadInfo.threadName || "Tên không tồn tại";
-    let nameThread = global.data.threadInfo.get(event.threadID).threadName || "Tên không tồn tại"; 
+
+    let nameThread = "Tên không tồn tại";
+    try {
+      let threadInfo = await api.getThreadInfo(event.threadID);
+      nameThread = threadInfo.threadName || nameThread;
+    } catch(e) {}
   
-    let threadInfo = await api.getThreadInfo(event.threadID);
-    nameThread =threadInfo.threadName;
     const nameUser = global.data.userName.get(event.author) || await Users.getNameUser(event.author);
-  
-    console.log(nameThread)
   
     var formReport = "『 🌸 』 Thông báo Thêm/Kick 『 🌸 』" +
       "\n\n『 🌸 』 Box name: " + nameThread +
       "\n『 🌸 』 Thread ID: " + event.threadID +
-      "\n『 🌸 』] Hành động: {task}" +
+      "\n『 🌸 』 Hành động: {task}" +
       "\n『 🌸 』 Tên người dùng: " + nameUser +
       "\n『 🌸 』 UserID: " + event.author +
       "\n\n『 🌸 』 Time: " + time + "",
@@ -41,7 +40,6 @@ module.exports.config = {
     switch (event.logMessageType) {
       case "log:thread-name": {
           let newName = event.logMessageData.name || "Tên không tồn tại";
-          //task = "Người dùng thay đổi tên nhóm thành " + newName + "";
           await Threads.setData(event.threadID, {name: newName});
           break;
       }
@@ -54,13 +52,12 @@ module.exports.config = {
           if(event.senderID == botID) return;
           const data = (await Threads.getData(event.threadID)).data || {};
           data.banned = true;
-          var reason = "[『 🌸 』 Kích bot tự do, không xin phép";
+          var reason = "『 🌸 』 Kích bot tự do, không xin phép";
           data.reason = reason || null;
           data.dateAdded = time;
           await Threads.setData(event.threadID, { data });
           global.data.threadBanned.set(event.threadID, { reason: data.reason, dateAdded: data.dateAdded });
-  
-          task = "『 🌸 』 Người dùng đã kick bot ra khỏi nhóm"
+          task = "『 🌸 』 Người dùng đã kick bot ra khỏi nhóm";
         }
         break;
       }
@@ -70,10 +67,9 @@ module.exports.config = {
   
     if (task.length == 0) return;
   
-    formReport = formReport
-      .replace(/\{task}/g, task);
+    formReport = formReport.replace(/\{task}/g, task);
   
-    return api.sendMessage(formReport, global.config.ADMINBOT[0], (error, info) => {
+    return api.sendMessage(formReport, global.config.ADMINBOT[0], (error) => {
       if (error) return logger(formReport, "Logging Event");
     });
   }
